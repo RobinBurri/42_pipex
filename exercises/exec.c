4482772730ea41baa29c1816b99ccf47b6590409
Robin Burri <robinburri@tutanota.com>
2021-12-07 13:06:50 +0100

    fifo

--- exercises/exec.c
+++ exercises/exec.c
@@ -3,13 +3,35 @@
 #include <unistd.h>
 #include <errno.h>
 
+
+// Executing commands in C
+// int main(void)
+// {
+// 	//execl("/sbin/ping", "/sbin/ping", "google.com", NULL);
+// 	// OR
+// 	//char *arr[] = { "ping", "google.com", NULL};
+// 	//execvp("ping", arr);
+// 	//OR
+// 	execlp("ping", "ping", "google.com", NULL);
+// 	return (0);
+// }
+
+
+// Executing commands in child process while the main waits
 int main(void)
 {
-	//execl("/sbin/ping", "/sbin/ping", "google.com", NULL);
-	// OR
-	//char *arr[] = { "ping", "google.com", NULL};
-	//execvp("ping", arr);
-	//OR
-	execlp("ping", "ping", "google.com", NULL);
+	int pid = fork();
+	if (pid == -1)
+		return (1);
+	if (pid == 0)
+	{
+		execlp("ping", "ping", "-c", "3", "google.com", NULL);
+	}
+	else
+	{
+		wait(NULL);
+		printf("Some post processes here, main.");
+	}
+	
 	return (0);
 }
---
+++ exercises/fifo.c
@@ -0,0 +1,29 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
+int main()
+{
+	int fd;
+	if (mkfifo("myfifo1", 0777) == -1)
+	{
+		if (errno != EEXIST)
+		{
+			printf("Couldn't create fifo file\n");
+			return (1);
+		}
+	}
+	fd = open("myfifo1", O_WRONLY);
+	if (fd == -1)
+		return (2);
+	int x = 97;
+	if (write(fd, &x, sizeof(x)) == -1)
+		return (3);
+	close(fd);
+	return 0;
+}
